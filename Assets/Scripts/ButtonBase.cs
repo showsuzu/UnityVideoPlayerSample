0471ae0e8d859826fa961bb3cd92b11486b7e7e1
suzukisy <suzukisy@casio.co.jp>
2018-01-03 12:39:11 +0900

    add new file

---
+++ Assets/Scripts/ButtonBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBase : MonoBehaviour {
+	// ボタンのベースクラス
+	public ButtonBase button;
+
+	public void OnClick(){
+		if (button == null)
+		{
+			throw new System.Exception("Button instance is null!!");
+		}
+		// 自身のオブジェクト名を渡す
+		button.OnClick(this.gameObject.name);
+	}
+
+	protected virtual void OnClick(string objectName)
+	{
+		// 呼ばれることはない
+		Debug.Log("Base Button");
+	}
+}
---
+++ Assets/Scripts/VedeoPlayButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VedeoPlayButton : ButtonBase {
+	// ButtonBaseクラスを継承したVideo Player Sceneのボタン処理
+	protected override void OnClick(string objectName)
+	{
+		// 渡されたオブジェクト名で処理を分岐
+		if ("PlayButton".Equals (objectName)) {
+			// PlayButton 用処理
+			this.VideoStart ();
+		} else {
+			throw new System.Exception("Not implemented!!");
+		}
+	}
+
+	public void VideoStart(){
+		// ボタンクリック処理
+		GameObject.Find ("VideoPlayerSceneController").GetComponent<VideoPlayController> ().VideoStart ();
+	}
+}
